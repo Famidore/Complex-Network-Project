2335c70e486fe4512302838ba1444317b4d02349
Famidore <lokoroko456@gmail.com>
2022-10-27 21:33:49 +0200

    created template

--- main.js
+++ main.js
@@ -1,7 +1,34 @@
+var tempX, tempY, tempSpeedX, tempSpeedY;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+
+  // temp setup
+  tempX = windowWidth / 2;
+  tempY = windowHeight / 2;
+  tempSpeedX = 3;
+  tempSpeedY = 3;
 }
 
 function draw() {
   background(51);
+  temp();
+}
+
+
+function temp() {
+  textSize(100);
+  textAlign(CENTER);
+  fill(255, 0, 150);
+  text("Work in Progress", tempX, tempY);
+
+  tempX += tempSpeedX;
+  tempY += tempSpeedY;
+
+  if (tempX >= windowWidth - 400 || tempX <= 400) {
+    tempSpeedX *= -1;
+  }
+  if (tempY >= windowHeight - 50 || tempY <= 50) {
+    tempSpeedY *= -1
+  }
 }
