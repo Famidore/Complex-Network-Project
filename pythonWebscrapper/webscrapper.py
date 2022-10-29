2be5d40420aed94687ac0276ae3e557ccdf528ab
Famidore <lokoroko456@gmail.com>
2022-10-30 00:40:46 +0200

    t

--- pythonWebscrapper/webscrapper.py
+++ pythonWebscrapper/webscrapper.py
@@ -9,17 +9,18 @@ def getRecommendations(url):
     # print(pageContent)
     soup = BeautifulSoup(pageContent, 'lxml')
     title = soup.find_all(class_="_3BFvyrImF3et_ZF21Xd8SC")
-    if title == []:
-        print('Extended the limit \n Waiting...\n')
-        time.sleep(60)
-    else:
-        print(f'\n Recommendations for {url}: \n')
-        # print(title)
-        for i in title:
-            print(i.text)
-            listed.append(i.text)
-            time.sleep(2)
-    return listed
+    while True:
+        if title == []:
+            print('Extended the limit \n Waiting...\n')
+            time.sleep(60)
+        else:
+            print(f'\n Recommendations for {url}: \n')
+            # print(title)
+            for i in title:
+                print(i.text)
+                listed.append(i.text)
+                time.sleep(2)
+        return listed
     # print(pageContent.content)
     # print(title)
 
