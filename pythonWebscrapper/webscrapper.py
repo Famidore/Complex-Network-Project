4b36eecab0a139f4dda0a4397f8837f386e59e1f
Famidore <lokoroko456@gmail.com>
2022-10-30 01:31:41 +0200

    kinda-fixed scraper

--- pythonWebscrapper/webscrapper.py
+++ pythonWebscrapper/webscrapper.py
@@ -4,32 +4,31 @@ import requests
 import time
 
 def getRecommendations(url):
+    print(f'\nLooking at: {url}\n')
     pageContent = requests.get(url).text
     listed = []
-    # print(pageContent)
     soup = BeautifulSoup(pageContent, 'lxml')
-    # title = soup.find_all(class_="_3BFvyrImF3et_ZF21Xd8SC")
-    while True:
-        title = soup.find_all(class_="_3BFvyrImF3et_ZF21Xd8SC")
-        if title == []:
-            print('Extended the limit \n Waiting...\n')
-            for i in range(6):
-                print(f'Waiting {60 - i*10} sec...')
-                time.sleep(10)
-        else:
-            print(f'\n Recommendations for {url}: \n')
-            # print(title)
-            for i in title:
-                print(i.text)
-                listed.append(i.text)
-                time.sleep(2)
-        return listed
-    # print(pageContent.content)
-    # print(title)
+    title = soup.find_all(class_="_3BFvyrImF3et_ZF21Xd8SC")
+    if title == []:
+        print('\nExtended the limit \n Waiting...\n')
+        for i in range(5):
+            print(f'Waiting {i*2 + 2} sec...')
+            time.sleep(2)
+    else:
+        print(f'\n Recommendations for {url}: \n')
+        for i in title:
+            print(i.text)
+            listed.append(i.text)
+            time.sleep(2)
+    return listed
 
 
+starter =  input('Submit a reddit community link: ')
+def doStuff(tag):
+    if tag != []:
+        for item in tag:
+            getRecommendations(f'https://www.reddit.com/{item}')
+            doStuff(item)
 
-temp = getRecommendations(input('Submit a reddit community link: '))
-# print(temp)
-for i in temp:
-    getRecommendations(f'https://www.reddit.com/{i}')
+while True:
+    doStuff(getRecommendations(starter))
