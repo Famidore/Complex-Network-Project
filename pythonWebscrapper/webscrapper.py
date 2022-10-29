f9d41eb67bd6d999bea932290953af1f8d5a31a4
Famidore <lokoroko456@gmail.com>
2022-10-30 00:34:57 +0200

    added timer

--- pythonWebscrapper/webscrapper.py
+++ pythonWebscrapper/webscrapper.py
@@ -9,19 +9,22 @@ def getRecommendations(url):
     # print(pageContent)
     soup = BeautifulSoup(pageContent, 'lxml')
     title = soup.find_all(class_="_3BFvyrImF3et_ZF21Xd8SC")
-    print(f'\n Recommendations for {url}: \n')
-    # print(title)
-    for i in title:
-        print(i.text)
-        listed.append(i.text)
-        time.sleep(2)
+    if title == []:
+        print('Waiting...')
+    else:
+        print(f'\n Recommendations for {url}: \n')
+        # print(title)
+        for i in title:
+            print(i.text)
+            listed.append(i.text)
+            time.sleep(2)
     return listed
     # print(pageContent.content)
     # print(title)
 
 
 
-temp = getRecommendations(input())
+temp = getRecommendations(input('Submit a reddit community link: '))
 # print(temp)
 for i in temp:
     getRecommendations(f'https://www.reddit.com/{i}')
