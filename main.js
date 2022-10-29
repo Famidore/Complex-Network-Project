388faa09be385927669098b1f957b898c21fa70a
Famidore <lokoroko456@gmail.com>
2022-10-30 00:12:48 +0200

    commiting webscraper

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
+let button, input;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  tempSetup(windowWidth, windowHeight)
+  tempSetup(windowWidth, windowHeight);
+  inputSetup();
+
 }
 
 function draw() {
   background(51);
   temp();
+  
+  textSize(20);
+  textAlign(LEFT);
+  text('Paste a video link below', input.x, input.y - 20);
+}
+
+function inputSetup() {
+  input = createInput();
+  input.style('color', '#8400ff');
+  input.position(50, windowHeight - 50);
+
+  button = createButton('submit');
+  button.style('background-color', color(200));
+  button.position(input.x + input.width, windowHeight - 50);
+  button.mousePressed(gatherURL);
+}
+
+function gatherURL() {
+  
+  print('hello');
+
 }
---
+++ pythonWebscrapper/webscrapper.py
@@ -0,0 +1,27 @@
+from mimetypes import init
+from bs4 import BeautifulSoup
+import requests
+import time
+
+def getRecommendations(url):
+    pageContent = requests.get(url).text
+    listed = []
+    # print(pageContent)
+    soup = BeautifulSoup(pageContent, 'lxml')
+    title = soup.find_all(class_="_3BFvyrImF3et_ZF21Xd8SC")
+    print(f'\n Recommendations for {url}: \n')
+    # print(title)
+    for i in title:
+        print(i.text)
+        listed.append(i.text)
+        time.sleep(2)
+    return listed
+    # print(pageContent.content)
+    # print(title)
+
+
+
+temp = getRecommendations('https://www.reddit.com/r/rodentintercourse/')
+print(temp)
+for i in temp:
+    getRecommendations(f'https://www.reddit.com/{i}')
