caa8826b9356e7853a02f436e6d5261320393740
Famidore <lokoroko456@gmail.com>
2022-11-10 17:01:19 +0100

    added searching for population on subReddits

--- pythonWebscrapper/webscrapper.py
+++ pythonWebscrapper/webscrapper.py
@@ -2,29 +2,30 @@
 from mimetypes import init
 from bs4 import BeautifulSoup
 import time
-import json
 import requests
 
-lookedAt =[]
+lookedAt = []
 
 
 def getRecommendations(url):
     if url not in lookedAt:
         lookedAt.append(url)
         print(f'\nLooking at: {url}\n')
-        
+
         tries = 0
         title = None
         while not title:
-            tries+=1
-            pageContent = requests.get(url, timeout=3).text
+            tries += 1
+            pageContent = requests.get(url, timeout=5).text
             listed = []
             soup = BeautifulSoup(pageContent, 'html5lib')
             title = soup.find_all(class_="_3BFvyrImF3et_ZF21Xd8SC")
-            for i in title:
-                print("\t" + i.text)
-                listed.append(i.text)
-                time.sleep(2)
+            members = soup.find_all(class_="e0X82913OdCTkFf2HU-WL")
+            if title:
+                for i, j in zip(title, members):
+                    print("\t" + i.text + "\t" + j.text)
+                    listed.append([i.text, j.text.split(" ")[0]])
+                    time.sleep(1)
             if tries > 15:
                 print("Could not find recommendations in " + url)
                 break
@@ -43,12 +44,13 @@ starter = input('Submit a reddit community link: ')
 def doStuff(sub):
     for start in sub:
         available = getRecommendations(f'https://www.reddit.com/{start}')
-    input(available)
+    # input(available)
     while available:
+        print(available[0], type(available[0]))
         for i in available:
-            for j in getRecommendations(f'https://www.reddit.com/{i}'):
-                available.append(j)
-    
+            for j in getRecommendations(f'https://www.reddit.com/{i[0]}'):
+                if j:
+                    available.append(j)
 
 
 def writeData(new_data, filename='data.txt'):
@@ -59,7 +61,6 @@ def writeData(new_data, filename='data.txt'):
             print("\n Saved the data \n")
         else:
             print("\n Key already exists \n")
-
 
 
 doStuff(getRecommendations(starter))
